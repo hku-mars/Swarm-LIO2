834cd610e03be359378283202a1586e4dd8397b8
zfc-zfc <625257073@qq.com>
2025-05-06 15:44:33 +0800

    Delete useless codes in CmakeLists.txt in udp_bridge.

--- swarm_lio/record_teammate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rosbag record /quad0_pcl_render_node/sensor_cloud /quad_0/imu /quad_0/lidar_slam/odom /quad0_odom_visualization/path /quad1_pcl_render_node/sensor_cloud /quad_1/imu /quad_1/lidar_slam/odom /quad1_odom_visualization/path /quad2_pcl_render_node/sensor_cloud /quad_2/imu /quad_2/lidar_slam/odom /quad2_odom_visualization/path /quadstate_to_teammate
-
